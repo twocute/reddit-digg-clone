4032f5fb64cf0540ded5ed952fd73911cffa4c6a
waihong <waihong.upwork@outlook.com>
2017-03-30 11:04:06 +0800

    Commented out FontAwesome, jQuery, Bootstrap and meta. We don't need them yet.

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -8,12 +8,12 @@
     <title>@yield('title')</title>
 
     <!-- Meta -->
-    <meta name="description" content="@yield('meta_description', 'meow')">
-    <meta name="author" content="@yield('meta_author', 'placeholder')">
+    <!--<meta name="description" content="@yield('meta_description', 'meow')">
+    <meta name="author" content="@yield('meta_author', 'placeholder')">-->
     @yield('meta')
 
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <!--<link href="https://fonts.googleapis.com/css?family=Roboto:300,400" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />-->
     <!-- Styles -->
     @yield('styles')
 </head>
@@ -25,8 +25,8 @@
         @yield('content')
     </div>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>-->
     @yield('scripts')
 </body>
 </html>
