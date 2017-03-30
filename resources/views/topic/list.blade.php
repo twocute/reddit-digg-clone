a01c4937d3be25b3d37448422829debaa4466cd6
waihong <waihong.upwork@outlook.com>
2017-03-30 12:25:04 +0800

    Move the buttons to the front

--- resources/views/topic/list.blade.php
+++ resources/views/topic/list.blade.php
@@ -4,20 +4,20 @@
 <div>
     <table class="table table-hover">
         <tr>
+            <th width="20%">Actions</th>
             <th width="60%">Topic</th>
             <th width="10%">Upvote(s)</th>
             <th width="10%">Downvote(s)</th>
-            <th width="20%">Actions</th>
         </tr>
         @foreach ($topics as $topic)
             <tr>
-                <td>{{ $topic['topic'] }}</td>
-                <td>{{ $topic['upvotes'] }}</td>
-                <td>{{ $topic['downvotes'] }}</td>
                 <td>
                     <a class="btn btn-default" href="/topic/upvote/{{ $topic['id'] }}">Upvote</a>
                     <a class="btn btn-default" href="/topic/downvote/{{ $topic['id'] }}">Downvote</a>
                 </td>
+                <td>{{ $topic['topic'] }}</td>
+                <td>{{ $topic['upvotes'] }}</td>
+                <td>{{ $topic['downvotes'] }}</td>
             </tr>
         @endforeach
     </table>
