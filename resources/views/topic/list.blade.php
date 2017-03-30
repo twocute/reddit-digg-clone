3a28c6dbc39b3a7acadb6ab61f65c0ecc29fc848
waihong <waihong.upwork@outlook.com>
2017-03-30 11:58:37 +0800

    Just a quick view to list all the topics

--- resources/views/topic/list.blade.php
+++ resources/views/topic/list.blade.php
@@ -1,1 +1,20 @@
-to list
+@extends('layout.main')
+
+@section('content')
+<div>
+    <table>
+        <tr>
+            <th>Topic</th>
+            <th>Upvote(s)</th>
+            <th>Downvote(s)</th>
+        </tr>
+        @foreach ($topics as $topic)
+            <tr>
+                <td>{{ $topic['topic'] }}</td>
+                <td>{{ $topic['upvotes'] }}</td>
+                <td>{{ $topic['downvotes'] }}</td>
+            </tr>
+        @endforeach
+    </table>
+</div>
+@endsection
