cc59aac5eec6ecbd485df7c4bb7c4ed1475a08e5
waihong <waihong.upwork@outlook.com>
2017-03-30 12:10:08 +0800

    Add buttons to upvote/downvote a topic

--- resources/views/topic/list.blade.php
+++ resources/views/topic/list.blade.php
@@ -7,12 +7,17 @@
             <th>Topic</th>
             <th>Upvote(s)</th>
             <th>Downvote(s)</th>
+            <th>Actions</th>
         </tr>
         @foreach ($topics as $topic)
             <tr>
                 <td>{{ $topic['topic'] }}</td>
                 <td>{{ $topic['upvotes'] }}</td>
                 <td>{{ $topic['downvotes'] }}</td>
+                <td>
+                    <a href="/topic/upvote/{{ $topic['id'] }}">Upvote</a>
+                    <a href="/topic/downvote/{{ $topic['id'] }}">Downvote</a>
+                </td>
             </tr>
         @endforeach
     </table>
