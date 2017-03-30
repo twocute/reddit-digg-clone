6eccb08257688317929a6c3158be8fbb4dad0c28
waihong <waihong.upwork@outlook.com>
2017-03-30 11:07:20 +0800

    Just a quick form for user to add a topic

--- resources/views/topic/add.blade.php
+++ resources/views/topic/add.blade.php
@@ -1,1 +1,10 @@
-to add
+@extends('layout.main')
+
+@section('content')
+<div>
+    <form method="POST">
+        <input type="text" name="topic">
+        <button type="submit">Add New Topic!</button>
+    </form>
+</div>
+@endsection
